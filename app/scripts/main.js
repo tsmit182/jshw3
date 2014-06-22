c0c03e6c00965f7e02982496909a91a4fb4785b9
Taylor Smith <tsmit182@gmail.com>
2014-06-22 16:44:13 -0400

    console logging title of item paid for in GBP

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,3 +10,10 @@ var sumPrice = eachPrice.reduce(function (a, b) {
 var actAvg = (sumPrice / eachPrice.length);
 var rndAvg = actAvg.toPrecision(4)
 console.log("The average price is $" + rndAvg)
+
+
+//2
+var pullPound = items.map(function (a) {
+	if (a.currency_code === 'GBP')
+		console.log (a.title)
+	});
