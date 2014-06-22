f6c18f8ec1a4458c6379a7991e4a2ab2fe275af4
Taylor Smith <tsmit182@gmail.com>
2014-06-22 16:32:42 -0400

    console logging toPrecision average for question one

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,11 @@
-items.forEach(function (a) {
-  
-}
+//1
+	return a.price;
+});
+
+var sumPrice = eachPrice.reduce(function (a, b) {
+	return a + b;
+});
+
+var actAvg = (sumPrice / eachPrice.length);
+var rndAvg = actAvg.toPrecision(4)
+console.log(rndAvg)
